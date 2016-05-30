62473bd83635527f70eb681241092325ef252b2a
sli <sli@movoto.com>
2016-05-30 18:48:02 +0800

    fix errors.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import './assets/styles/base.scss';
-import './assets/scripts/base';
-import './assets/scripts/index';
-
-import Vue from 'vue'
-import App from './App'
-
+// css or scss
+import "bootstrap/dist/css/bootstrap.css";
+import "./assets/styles/index.scss";
+import "vue-strap";
+import "./assets/scripts/base";
+import "./assets/scripts/index";
+import Vue from "vue";
+import App from "./App";
+// import store from "./vuex/store";
 
 /* eslint-disable no-new */
 new Vue({
+  // store, // inject store to all children
   el: 'body',
-  components: { App }
-})
+  vuex: {
+    getters: {
+      cls: (state) => 'flat-blue'
+    }
+  },
+  components: {App}
+});
